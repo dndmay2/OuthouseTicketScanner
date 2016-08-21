9462625b443403a6db697eaa1264931d37176f71
Derek May <derekmay@dereks-new-macbook-pro.local>
2016-08-20 23:51:41 -0500

    Initial Commit

---
+++ Outhouse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Outhouse
+//
+//  Created by Derek May on 8/20/16.
+//  Copyright © 2016 Derek May. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
